3b2e407d137d4fe030c11dc7b6844e6469f0bbd4
Lee <94841127+galaxian@users.noreply.github.com>
2024-09-09 10:32:33 +0900

    feat : 게시글 작성 service 로직 구현

--- src/main/java/com/example/test/Item/service/ItemService.java
+++ src/main/java/com/example/test/Item/service/ItemService.java
@@ -1,7 +1,11 @@
 package com.example.test.Item.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.test.Item.domain.Item;
+import com.example.test.Item.dto.request.ItemReqDto;
+import com.example.test.Item.dto.response.ItemResDto;
 import com.example.test.Item.repository.ItemRepository;
 
 @Service
@@ -11,5 +15,14 @@ public class ItemService {
 
 	public ItemService(ItemRepository itemRepository) {
 		this.itemRepository = itemRepository;
+	}
+
+	@Transactional
+	public ItemResDto createPost(ItemReqDto reqDto) {
+		Item item = reqDto.toEntity();
+
+		Item savedItem = itemRepository.save(item);
+
+		return new ItemResDto(savedItem);
 	}
 }
