87a75a31a999cd8087c80ce0f689f1327f7e5af5
Lee <94841127+galaxian@users.noreply.github.com>
2024-09-09 10:57:07 +0900

    feat : 게시글 삭제 controller 구현

--- src/main/java/com/example/test/Item/controller/ItemController.java
+++ src/main/java/com/example/test/Item/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.example.test.Item.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.test.Item.dto.request.ItemReqDto;
+import com.example.test.Item.dto.response.ItemDeleteResDto;
 import com.example.test.Item.dto.response.ItemResDto;
 import com.example.test.Item.service.ItemService;
 
@@ -38,6 +40,11 @@ public class ItemController {
 	@PutMapping("/post/{id}")
 	public ResponseEntity<ItemResDto> updatePost(@PathVariable Long id, @RequestBody ItemReqDto itemReqDto) {
 		ItemResDto response = itemService.updatePost(id, itemReqDto);
+		return ResponseEntity.ok(response);
+	}
+	@DeleteMapping("/post/{id}")
+	public ResponseEntity<ItemDeleteResDto> deletePost(@PathVariable Long id) {
+		ItemDeleteResDto response = itemService.deletePost(id);
 		return ResponseEntity.ok(response);
 	}
 }
