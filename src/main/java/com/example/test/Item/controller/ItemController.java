8dee90df2556726ddd6ed35b570c0d7613fa976c
Lee <94841127+galaxian@users.noreply.github.com>
2024-09-09 10:39:28 +0900

    feat : 게시글 작성 controller 구현

--- src/main/java/com/example/test/Item/controller/ItemController.java
+++ src/main/java/com/example/test/Item/controller/ItemController.java
@@ -1,7 +1,12 @@
 package com.example.test.Item.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.test.Item.dto.request.ItemReqDto;
+import com.example.test.Item.dto.response.ItemResDto;
 import com.example.test.Item.service.ItemService;
 
 @RestController
@@ -11,5 +16,11 @@ public class ItemController {
 
 	public ItemController(ItemService itemService) {
 		this.itemService = itemService;
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<ItemResDto> createPost(@RequestBody ItemReqDto itemReqDto) {
+		ItemResDto response = itemService.createPost(itemReqDto);
+		return ResponseEntity.ok(response);
 	}
 }
